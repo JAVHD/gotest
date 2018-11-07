9dc957ceadffaf1de7ca564fd5c82ad5f6f49051
bear <593788753@qq.com>
2018-11-07 10:52:46 +0800

    test

---
+++ goversion.go
@@ -0,0 +1,18 @@
+/*
+* @Author: bear
+* @Date:   2018-10-20 17:41:43
+* @Last Modified by:   bear
+* @Last Modified time: 2018-10-20 17:42:54
+ */
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+
+	fmt.Println(runtime.Version())
+}
